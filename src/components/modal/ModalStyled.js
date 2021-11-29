0b5b67bc5a2b471ccea0de4808b7158bcb9fe2ea
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-11-29 02:37:50 +0200

    change fs modal

--- src/components/modal/ModalStyled.js
+++ src/components/modal/ModalStyled.js
@@ -4,11 +4,13 @@ const ModalStyled = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  padding: 20px;
+  padding: 30px;
+  text-align: center;
   height: 100vh;
   width: 100%;
   background-color: #ffffff;
   z-index: 100;
+
   .modal_close_icon {
     position: absolute;
     top: 10px;
--- src/components/navigation/NavigationStyled.js
+++ src/components/navigation/NavigationStyled.js
@@ -7,9 +7,15 @@ const NavigationStyled = styled.nav`
 
   .nav_link {
     font-weight: 700;
-    font-size: 20px;
-    color: #37afff;
+    font-size: 50px;
+    color: #437fff;
     text-transform: capitalize;
+    text-shadow: rgb(202, 200, 200) 0px 1px 0px, rgb(199, 197, 197) 0px 2px 0px,
+      rgb(187, 186, 186) 0px 3px 0px, rgb(199, 197, 197) 0px 4px 0px,
+      rgb(199, 197, 197) 0px 5px 0px, rgba(0, 0, 0, 0.2) 0px 6px 1px,
+      rgba(0, 0, 0, 0.1) 0px 0px 5px, rgba(0, 0, 0, 0.1) 0px 1px 3px,
+      rgba(0, 0, 0, 0.1) 0px 3px 5px, rgba(0, 0, 0, 0.2) 0px 5px 10px,
+      rgba(0, 0, 0, 0.3) 0px 10px 10px, rgba(0, 0, 0, 0.1) 0px 20px 20px;
   }
 
   .active_Link {
@@ -22,6 +28,10 @@ const NavigationStyled = styled.nav`
     }
     .nav_list_item:not(:last-child) {
       margin: 0 10px 0 0;
+    }
+    .nav_link {
+      font-size: 20px;
+      text-shadow: none;
     }
   }
 `;
